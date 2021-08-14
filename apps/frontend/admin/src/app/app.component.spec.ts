f1f67cfde8043c64e1ff832cca4bd5676b8bba61
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-08-15 00:46:37 +0300

    feat(IDE): delete default html app.component
    delete default html app.component
    
    Closes #2

--- apps/frontend/admin/src/app/app.component.spec.ts
+++ apps/frontend/admin/src/app/app.component.spec.ts
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to frontend-admin!'
+      'Welcome to frontend-admin!',
     );
   });
 });
