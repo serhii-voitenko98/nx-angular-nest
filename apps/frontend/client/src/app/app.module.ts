62e3d04979d5c718f288a3eae244fda7a00bee39
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-08-15 19:16:11 +0300

    feat(scss): set import to app
    #10

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MatCardModule, MatButtonModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
