925125f67fb56c9b40b2cae3e21b82fb16794564
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-08-15 23:36:18 +0300

    feat(api-auth): add auth module
    * change auth.service
    
    #12

--- apps/backend/api/src/app/auth/services/auth.service.ts
+++ apps/backend/api/src/app/auth/services/auth.service.ts
@@ -1,14 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ISignAuthPayload, ISignAuthResponse } from '@nx-angular-nest/shared/data-access/interfaces';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../../users/services/user.service';
+import { PasswordService } from './password.service';
+import { UserEntity } from '../../users/entities/user.entity';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class AuthService {
-  login(signAuthPayload: ISignAuthPayload): Promise<ISignAuthResponse> {
-    return
-    // {
-    //   accessToken: // JWT
-    //   expiresIn: // JWT
-    //   id: // user service
-    // };
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+    private readonly passwordService: PasswordService,
+  ) {
+  }
+
+  async validateUser(username: string, pass: string): Promise<Omit<UserEntity, 'password'>> {
+    const user = await this.userService.findOneByUserName(username);
+    const isValid = user ? await this.passwordService.compareHash(pass, user.password) : false;
+
+    if (isValid) {
+      delete user.password;
+
+      return user;
+    }
+
+    return null;
+  }
+
+  async login(signAuthPayload: ISignAuthPayload): Promise<ISignAuthResponse> {
+    const user = await this.validateUser(signAuthPayload.username, signAuthPayload.password);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = {
+      username: user.username,
+      id: user.id,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+      expiresIn: new Date(environment.jwt.expiresIn).getDate(),
+      id: user.id,
+    };
   }
 }
