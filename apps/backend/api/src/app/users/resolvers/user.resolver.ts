686c987cf783d08cc6c54b100c0be60807b90675
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-08-16 01:28:57 +0300

    fix(user-resolver): add decorators
    * add decorators: Query, UseGuards
    
    #12

--- apps/backend/api/src/app/users/resolvers/user.resolver.ts
+++ apps/backend/api/src/app/users/resolvers/user.resolver.ts
@@ -1,7 +1,10 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+
 import { UserService } from '../services/user.service';
 import { CurrentUser } from '../decorators/user.decorator';
 import { UserEntity } from '../entities/user.entity';
+import { GqlAuthGuard } from '../guards/gql.auth-guard';
 
 @Resolver('User')
 export class UserResolver {
@@ -10,6 +13,8 @@ export class UserResolver {
   ) {
   }
 
+  @Query('user')
+  @UseGuards(GqlAuthGuard)
   async whoAmI(@CurrentUser() user: UserEntity) {
     return this.userService.findOneById(user.id);
   }
