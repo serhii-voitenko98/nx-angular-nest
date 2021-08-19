94aeb2891f379596ae95be854404e9abde3e49e0
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-08-20 00:11:53 +0300

    feat(root-store.module.ts): add router-state-serializer [WiP] * add store-router-state-serializer
    #20

--- libs/frontend-client/data-access/store/src/lib/root-store.module.ts
+++ libs/frontend-client/data-access/store/src/lib/root-store.module.ts
@@ -4,12 +4,15 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { RouterModule } from '@angular/router';
 import { reducers } from './+state/root-reducer';
 import { rootInitialState } from './+state/root-initial-state';
+import { StoreRouterStateSerializer } from './services/store-router-state-serializer-service';
 
 @NgModule({
   imports: [
     RouterModule,
     StoreModule.forRoot(reducers, { initialState: rootInitialState }),
-    StoreRouterConnectingModule
+    StoreRouterConnectingModule.forRoot({
+      serializer: StoreRouterStateSerializer,
+    })
   ],
 })
 export class RootStoreModule {}
