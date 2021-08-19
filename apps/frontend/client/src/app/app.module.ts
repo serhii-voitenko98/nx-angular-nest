c96b89319c3e66dcd651bf3af9048ab5a5680f07
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-08-19 23:19:43 +0300

    feat(ws*client): add ssr
    * add app.browser.module.ts
      add  StoreDevtoolsModule
    * change appId
      serverApp => svvs-client
    * add RouterModule => app.module.ts
    * add main.browser.ts
    * change server.ts
      add {REQUEST, RESPONSE}
    * change tsconfig.app.json
      "files": ["src/main.browser.ts", "src/polyfills.ts"]
    * change angular.json
      add server section
    * add scripts => package.json
      frontend-client:ssr:build
      frontend-client:ssr:serve
    
    #17

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'frontend-client' }),
+    // RouterModule.forRoot([], { initialNavigation: 'enabled' })
+  ],
 })
 export class AppModule {
 }
